78b77a843d45985071bf6cf79daad7bfac89bceb
Seydina <dina3903@gmail.com>
2022-09-03 01:47:07 +0000

    update

---
+++ utils/constantes.js
@@ -0,0 +1,2 @@
+export const APP_SECRET = "eyJuYW1lIjoiRGluYSIsInNhaWQiOiJEb24ndCBrbm93IGhvdyB0byBnaXZlIHVwLiJ9.OM7adiluofr3yCGaByA22hOuIeUoPbWXTEyAKSXUdTY"
+export const API_URL = "https://tour-brown.vercel.app/"
